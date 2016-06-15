44eef41c079606c56be752909387b85942161c8e
Anil Pai <anilbpai@gmail.com>
2016-06-15 11:20:28 -0400

    minor changes

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,11 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
   $scope.link ={};
   $scope.addLink = function(){
     Links.addOne($scope.link);
     // Redirection: Set the location path to links
     $location.path('/links');
   };
+  $scope.signout = Auth.signout;
 });
